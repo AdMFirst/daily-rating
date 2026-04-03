31065c10592165d6ecb6c91425c8f91bbcd2755d
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2026-04-03 13:23:48 +0700

    Use daisyUI for styling

--- src/app/index/index.ts
+++ src/app/index/index.ts
@@ -9,7 +9,25 @@ import { Component, signal } from '@angular/core';
 export class Index {
 
   protected readonly textFromIndex = signal('Some mumbo jumbo long text');
-  protected readonly loremIpsum = signal('lorem ipsum dolor sit amet'.repeat(200));
+  protected readonly loremIpsum = signal('lorem ipsum dolor sit amet'.repeat(100));
+  
+  // Button click counter
+  protected readonly clickCount = signal(0);
+
+  // Fibonacci calculation
+  protected readonly fibonacci = (n: number): number => {
+    if (n <= 1) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+      [a, b] = [b, a + b];
+    }
+    return b;
+  };
+
+  // Handle button click
+  protected handleClick = () => {
+    this.clickCount.update(count => count + 1);
+  };
 
 
 }
