95bfc2e53c09c87d1532ef0923bf05b5f9bf0a97
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2026-04-07 15:20:34 +0700

    add timeline route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Pricing } from './pricing/pricing';
 import { Index } from './index';
+import { Timeline } from './timeline/timeline';
 
 export const routes: Routes = [
   { path: '', component: Index },
   { path: 'pricing', component: Pricing },
+  { path: 'timeline', component: Timeline },
 ];
