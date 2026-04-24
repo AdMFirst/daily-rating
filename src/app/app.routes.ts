9d22a03dc957acde2fce81158e1dfab0e253800f
AdMFirst <me@admfirst.my.id>
2026-04-24 14:35:01 +0700

    add 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { Pricing } from './pricing/pricing';
 import { Index } from './index';
 import { Timeline } from './timeline/timeline';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Index },
   { path: 'pricing', component: Pricing },
   { path: 'timeline', component: Timeline },
+  // Catch-all route for undefined URLs
+  { path: '**', component: NotFound },
 ];
